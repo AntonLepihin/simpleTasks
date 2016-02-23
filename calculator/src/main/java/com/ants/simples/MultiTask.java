33f030c4c571ba4e2083bcb2bebdaa07c998b3fa
Anton.Lepihin <Anton.Lepihin@trs-it.ru>
2016-02-23 18:15:10 +0300

    new task

---
+++ calculator/src/main/java/com/ants/simples/MultiTask.java
@@ -0,0 +1,7 @@
+package com.ants.simples;
+
+/**
+ * Created by anton on 2/23/16.
+ */
+public class MultiTask {
+}
---
+++ calculator/src/main/java/com/ants/simples/concurrent/TaskThread.java
@@ -0,0 +1,12 @@
+package com.ants.simples.concurrent;
+
+/**
+ * Created by anton on 2/23/16.
+ */
+public class TaskThread implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
